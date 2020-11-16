6a164ddeaa04c98898af09645fd80dab794522e3
minyitan <1913343670@qq.com>
2020-11-16 09:50:06 +0800

    change the singer page

--- app/Views/onesinger.php
+++ app/Views/onesinger.php
@@ -8,7 +8,11 @@
 		<p><strong>ID:</strong> {id}</p>
 		<p><strong>Name:</strong> {name}</p>
 		<p><strong>city:</strong> {city}</p>
-		<p><img src="/image/{image}"/></p>
+		<p><img src="/image/{image}" width="360px" height="240px"/></p>
+                <p><strong>favourit food:</strong> {food}</p>
+                <p><strong>age:</strong> {age}</p>
+                <p><strong>sex:</strong> {sex}</p>
+                <p><strong>Magnum opus:</strong> {song}</p>
 		<p><a href="/home">Home</a></p>
 	</body>
 </html>
